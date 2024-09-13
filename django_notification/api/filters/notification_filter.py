368d2114456c74653b3fdb9ee15f77d8b481102b
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-09-13 22:30:16 +0330

    :sparkles: feat(api): Add NotificationFilter Class for Enhanced Notification Querying
    Implemented the NotificationFilter class using django_filters to facilitate advanced querying of Notification objects through the API. This filter provides users with various options to narrow down notification results based on specific attributes.
    
    **Details:**
    
    - **Added Filters:**
      - **	imestamp:** Allows filtering notifications by a range of datetime values (rom and 	o).
      - **created_at:** Filters notifications based on the exact creation datetime.
      - **updated_at:** Filters notifications based on the exact last update datetime.
    
    - **Filter Fields:**
      - group__id: Filters by the ID of the group associated with the notification.
      - Recipient__id: Filters by the ID of the recipient.
      - status: Filters by the status of the notification.
      - email_sent: Filters by whether the notification email has been sent.
      - public: Filters by the public status of the notification.
    
    **Enhancements:**
    
    - **Granular Filtering:**
      - Added additional lookup options for 	imestamp to support more specific datetime queries.
    
    - **Documentation:**
      - Added docstrings to the NotificationFilter class to explain the purpose and functionality of each filter.
    
    **Notes:**
    
    Ensure that the filter fields and lookups match the actual fields and indexing in the Notification model for optimal performance.
    Closes #28

---
+++ django_notification/api/filters/notification_filter.py
@@ -0,0 +1,24 @@
+from django_filters import rest_framework as filters
+
+from django_notification.models.notification import Notification
+
+
+class NotificationFilter(filters.FilterSet):
+    """
+    Filter set for filtering notifications based on various criteria.
+    """
+
+    timestamp = filters.DateTimeFromToRangeFilter(
+        field_name="timestamp",
+        lookup_expr="range",
+        help_text="Filter notifications by timestamp within a specific range.",
+    )
+
+    class Meta:
+        model = Notification
+        fields = {
+            "group__id": ["exact"],
+            "recipient__id": ["exact"],
+            "status": ["exact"],
+            "public": ["exact"],
+        }
