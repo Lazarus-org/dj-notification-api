759fd9f1cd7844099cc3515e253989b9d1e9b735
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-09-13 21:54:26 +0330

    :sparkles::art: feat(pagination): Add Custom Pagination Class for Enforcing Limits
    - Added a new pagination class, DefaultLimitOffSetPagination, which extends LimitOffsetPagination from Django REST framework.
    - This class enforces minimum and maximum limits on the number of items returned per page.
    - It includes:
      - min_limit: Minimum allowed items per page (default: 1).
      - max_limit: Maximum allowed items per page (default: 100).
      - default_limit: Default number of items per page when no limit is specified (default: 10).
    - The get_limit method has been overridden to enforce these limits, ensuring the limit specified in the request is within the defined range and defaults to the specified value if invalid or not provided.
    
    **Benefits:**
    
    - Provides better control over pagination limits in API responses.
    - Ensures that clients cannot request an excessively large number of items, improving performance and usability.
    
    Closes #26

---
+++ django_notification/api/paginations/limit_offset_pagination.py
@@ -0,0 +1,61 @@
+from rest_framework.pagination import LimitOffsetPagination
+from typing import Optional
+
+from rest_framework.request import Request
+
+
+class DefaultLimitOffSetPagination(LimitOffsetPagination):
+    """
+    A custom LimitOffsetPagination class that enforces minimum and maximum limits
+    on the number of items returned per page.
+    """
+
+    # Minimum limit allowed in query parameters
+    min_limit: int = 1
+
+    # Maximum limit allowed in query parameters
+    max_limit: int = 100
+
+    # Default limit when no limit is specified
+    default_limit: int = 10
+
+    def get_limit(self, request: Request) -> Optional[int]:
+        """
+        Override the `get_limit` method to enforce minimum and maximum limits on the
+        number of items returned per page. The limit is extracted from the request's
+        query parameters.
+
+        Parameters:
+        -----------
+        request : Request
+            The request object containing query parameters with the pagination limit.
+
+        Returns:
+        --------
+        Optional[int]
+            The number of items to be returned per page, constrained by the defined
+            minimum and maximum limits, or the default limit if not specified.
+        """
+        limit = request.query_params.get(self.limit_query_param)
+
+        if limit:
+            try:
+                # Try to convert the limit to an integer
+                limit = int(limit)
+
+                # Enforce the minimum limit
+                if limit < self.min_limit:
+                    return self.default_limit
+
+                # Enforce the maximum limit
+                if limit > self.max_limit:
+                    return self.max_limit
+
+                return limit
+
+            except ValueError:
+                # If limit is not a valid integer, fall back to the default limit
+                pass
+
+        # Return the default limit if no valid limit is provided
+        return self.default_limit
