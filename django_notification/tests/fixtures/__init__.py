77cfceec5b6e563d06306e8700bcdba52cb4f022
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-09-14 22:48:50 +0330

    :sparkles::rotating_light: feat(tests): Add User, Group, and Permission Fixtures for Testing
     This commit introduces new fixtures to support testing with user, group, and permission-related models. These fixtures facilitate the creation of various instances needed for comprehensive testing.
    
     #### Added Fixtures:
     - **user**: Creates and returns a standard User instance with the username 'testuser', email 'testuser@example.com', and password '12345'.
    
     - **qs_user**: Creates and returns a secondary User instance with the username 'queryset_user', email 'queryset_user@example.com', and password '12345'.
    
     - **dmin_user**: Creates and returns a superuser with admin access, using the username 'admin' and password 'password'.
    
     - **nother_user**: Creates and returns an additional User instance with the username 'anotheruser', email 'anotheruser@example.com', and password '54321'.
    
     - **group**: Creates and returns a Group instance with the name 'testgroup'.
    
     - **qs_group**: Creates and returns a secondary Group instance with the name 'queryset_group'.
    
     - **permission**: Creates and returns a sample Permission object with the name 'Can add user' and codename 'add_user'.
    
     - **group_with_perm**: Creates and returns a Group instance with the provided Permission attached, named 'Admins'.
    
     - **user_content_type**: Retrieves and returns the ContentType instance for the User model.
    
     #### Improvements:
     - Added type annotations for each fixture for better clarity and type checking.
     - Enhanced docstrings to describe the purpose and return values of each fixture, including arguments where applicable.
    
    These fixtures will streamline the setup process for tests involving users, groups, and permissions, ensuring consistent and reliable test environments.
    Closes #41

---
+++ django_notification/tests/fixtures/__init__.py
@@ -0,0 +1,19 @@
+from .notification import (
+    notification,
+    notification_dict,
+    notifications,
+    notification_seen,
+    notification_recipient,
+    deleted_notification,
+)
+from .user import (
+    admin_user,
+    qs_user,
+    group,
+    qs_group,
+    group_with_perm,
+    permission,
+    user_content_type,
+    another_user,
+    user,
+)
