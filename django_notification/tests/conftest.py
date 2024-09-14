cacef7329944ecc5e14f7d2da035ab431ed885a5
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2024-09-14 23:12:30 +0330

    :sparkles::rotating_light: feat(tests): Add conftest file to import necessary modules for runnig tests

---
+++ django_notification/tests/conftest.py
@@ -0,0 +1,18 @@
+from django_notification.tests.setup import configure_django_settings
+from django_notification.tests.fixtures import (
+    user_content_type,
+    user,
+    another_user,
+    qs_user,
+    group,
+    qs_group,
+    group_with_perm,
+    permission,
+    notification,
+    notification_dict,
+    notifications,
+    notification_recipient,
+    notification_seen,
+    deleted_notification,
+    admin_user
+)
