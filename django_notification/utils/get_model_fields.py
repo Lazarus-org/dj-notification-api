6ad4804db1055211d64cdb59936e504fa8ba5e5f
MEHRSHAD MIRSHEKARY <mehrshad_mirshekary@email.com>
2025-01-13 22:45:38 +0330

    :sparkles: feat(utils): Add get_model_fields utility
    Added get_model_fields utilily that Used in django checks while running the app to validate the notification serializer fields

---
+++ django_notification/utils/get_model_fields.py
@@ -0,0 +1,16 @@
+from typing import List, Type
+
+from django.db.models import Model
+
+
+def get_model_fields(model: Type[Model]) -> List:
+    """
+    Get all field names for a given Django model.
+
+    Args:
+        model (Model): The Django model class.
+
+    Returns:
+        list: A list of all field names in the model.
+    """
+    return [field.name for field in model._meta.get_fields()]
